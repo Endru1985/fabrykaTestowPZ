30195be45c6dcbf350d3fe3badc9d4cf30494f60
Endru1985 <a.niewiadomski26@gmail.com>
2020-08-26 15:44:06 +0200

    homework_update

--- homeworks/homework_mod6.py
+++ homeworks/homework_mod6.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 
 driver = webdriver.Chrome('H:/programowanie/Kurs_testy_automatyczne/pythonProject/fabrykaTestowPZ/chromedriver.exe')
 
@@ -13,5 +14,8 @@ link.click()
 scroll = driver.find_element_by_xpath('//*[@id="content"]/div/div/div/div/div/div/div/section[8]/div/div/div/div/div/div/div/h2')
 driver.execute_script('arguments[0].scrollIntoView();', scroll)
 
-driver.save_screenshot('https://github.com/Endru1985/fabrykaTestowPZ/tree/branch1/homeworks/screenshot.png')
+driver.save_screenshot('C:/Users/Andrzej/Pictures/Screenshots/screenshot.png')
 
+time.sleep(5)
+
+driver.quit()
