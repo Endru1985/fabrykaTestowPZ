40a80c653b8e89a1e03ac65149baa3f662a9dd66
Endru1985 <a.niewiadomski26@gmail.com>
2020-08-26 15:07:46 +0200

    update

---
+++ homeworks/homework_mod6.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome('H:/programowanie/Kurs_testy_automatyczne/pythonProject/fabrykaTestowPZ/chromedriver.exe')
+
+url = 'https://fabrykatestow.pl'
+
+driver.get(url)
+driver.maximize_window()
+
+link = driver.find_element_by_xpath('//*[@id="menu-item-506"]/a')
+link.click()
+
+scroll = driver.find_element_by_xpath('//*[@id="content"]/div/div/div/div/div/div/div/section[8]/div/div/div/div/div/div/div/h2')
+driver.execute_script('arguments[0].scrollIntoView();', scroll)
+
+driver.save_screenshot('H:/programowanie/Kurs_testy_automatyczne/pythonProject/fabrykaTestowPZ//screenshot.png')
+
